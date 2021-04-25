3aa36e1b283df332a12e6f5d8d26d2cc3a004e17
hiepns97it <hiepns97it@gmail.com>
2021-04-25 13:19:56 +0700

    update add sub number state

--- src/srceen/Detail.js
+++ src/srceen/Detail.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
-import { StyleSheet,  Text, View } from 'react-native';
+import { StyleSheet,  Text, TouchableOpacity, View } from 'react-native';
 
 export default class Detail extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            number: 21
+        }
     }
 
+    onClick_Add() {
+        this.setState({
+            number: this.state.number + 1
+        })
+    }
+
+    onClick_Sub() {
+        this.setState({
+            number: this.state.number - 1
+        })
+    }
     render() {
         return(
-            <View>
-                <Text style={styles.container}>
-                    Detail
-                </Text>
+            <View style={styles.container}>
+                <View style={styles.container_hearder}>
+                    <Text style={styles.container_hearder_text}>
+                        {this.state.number}
+                    </Text>
+                </View>
+                <View style={styles.container_bottom}>
+                    <TouchableOpacity style={styles.container_bottom_add}
+                    onPress={() => {this.onClick_Add()}}
+                    >
+                        <Text style={styles.container_bottom_add_text}>+</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.container_bottom_sub}
+                    onPress={() => {this.onClick_Sub()}}
+                    >
+                        <Text style={styles.container_bottom_sub_text}>-</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
@@ -19,9 +47,45 @@ export default class Detail extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        backgroundColor: "yellow",
+        flex: 1
+    },
+    container_hearder: {
+        backgroundColor: "black",
+        flex: 9,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#fff"
-    }
+
+    },
+        container_hearder_text: {
+            color: "yellow",
+            fontSize: 70
+        },
+    container_bottom: {
+        backgroundColor: "green",
+        flex: 1,
+        flexDirection: "row"
+    },
+        container_bottom_add: {
+            backgroundColor: "red",
+            flex: 1,
+            borderRightWidth: 2, 
+            justifyContent: "center",
+            alignItems: "center",  
+        },
+            container_bottom_add_text: {
+                color: "yellow",
+                fontSize: 50
+            },
+        container_bottom_sub: {
+            backgroundColor: "red",
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+        },
+            container_bottom_sub_text: {
+                color: "yellow",
+                fontSize: 50
+            },
+        
 });
